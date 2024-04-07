1a211f1347296b6968b0f51071c67115e3e1a803
Tim de Korte <165480396+Timmehh90@users.noreply.github.com>
2024-04-08 00:52:01 +0200

    Feat: Add score tracking in playGame function
    At the end of the game the score will be displayed in console.

--- javascript.js
+++ javascript.js
@@ -7,30 +7,50 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "It's a draw";
+    return "draw";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    return "You win!";
+    return "win";
   } else {
-    return "You lose!";
+    return "lose";
   }
 }
 
 function playGame() {
   let result = "";
+  let computerScore = 0;
+  let playerScore = 0;
   let currentRound = 1;
+
   while (currentRound <= 5) {
     let computerChoice = getComputerChoice();
     let playerChoice = prompt(
       "Round " + currentRound + '\nChoose: "rock", "paper" or "scissors".'
     );
+    console.log(
+      "You chose: " + playerChoice + "\nThe computer chose: " + computerChoice
+    );
     result = playRound(playerChoice, computerChoice);
-    console.log(result);
+    if (result === "draw") {
+      console.log("You draw this round.");
+    } else if (result === "win") {
+      console.log("You win this round.");
+      playerScore++;
+    } else if (result === "lose") {
+      console.log("You lose this round.");
+      computerScore++;
+    } else {
+      console.log('Wrong input, choose: "rock", "paper" or "scissors".');
+      currentRound--;
+    }
     currentRound++;
   }
+  console.log(
+    "Your score: " + playerScore + "\nComputer Score: " + computerScore
+  );
 }
 
 // Main Loop
