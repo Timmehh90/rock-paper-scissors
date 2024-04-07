07746244e12fd8c74a89bf48e1178ed04894111e
Tim de Korte <165480396+Timmehh90@users.noreply.github.com>
2024-04-07 23:50:05 +0200

    Feat: Add playRound function
    playRound compares playerSelection with computerSelection with an if statement to return the round result.

--- javascript.js
+++ javascript.js
@@ -5,9 +5,25 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a draw";
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
+  ) {
+    return "You win!";
+  } else {
+    return "You lose!";
+  }
+}
+
 // Main Loop
 
-console.log(getComputerChoice());
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 // let playing = true;
 
 // while (playing) {}
