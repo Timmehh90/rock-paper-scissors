97d99287b8b7d7b42f8bff162076d7c43bb063b1
Tim de Korte <165480396+Timmehh90@users.noreply.github.com>
2024-04-17 10:06:10 +0200

    Refractor: make backup of old JS and clean current
    The code was not compatible with the UI, so moved the code to backup.js so
    i can troubleshoot better.

---
+++ backup.js
@@ -0,0 +1,58 @@
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
+  if (playerSelection === computerSelection) {
+    return "draw";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return "win";
+  } else {
+    return "lose";
+  }
+}
+
+function playGame() {
+  let result = "";
+  let computerScore = 0;
+  let playerScore = 0;
+  let currentRound = 1;
+
+  let playerChoice = prompt(
+    "Round " + currentRound + '\nChoose: "rock", "paper" or "scissors".'
+  );
+  console.log(
+    "You chose: " + playerChoice + "\nThe computer chose: " + computerChoice
+  );
+  result = playRound(playerChoice, computerChoice);
+  if (result === "draw") {
+    console.log("You draw this round.");
+  } else if (result === "win") {
+    console.log("You win this round.");
+    playerScore++;
+  } else if (result === "lose") {
+    console.log("You lose this round.");
+    computerScore++;
+  } else {
+    console.log('Wrong input, choose: "rock", "paper" or "scissors".');
+    currentRound--;
+  }
+  currentRound++;
+  console.log(
+    "Your score: " + playerScore + "\nComputer Score: " + computerScore
+  );
+
+  if (playerScore > computerScore) {
+    console.log("You win. Well played!");
+  } else if (playerScore < computerScore) {
+    console.log("You lose. Better luck next time!");
+  } else {
+    console.log("You tied all rounds!");
+  }
+}
--- javascript.js
+++ javascript.js
@@ -5,7 +5,9 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
+  console.log("Computer: " + computerSelection + " Player: " + playerSelection);
   if (playerSelection === computerSelection) {
     return "draw";
   } else if (
@@ -19,45 +21,16 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function playGame() {
-  let result = "";
-  let computerScore = 0;
-  let playerScore = 0;
-  let currentRound = 1;
-  let computerChoice = getComputerChoice();
-  let playerChoice = prompt(
-    "Round " + currentRound + '\nChoose: "rock", "paper" or "scissors".'
-  );
-  console.log(
-    "You chose: " + playerChoice + "\nThe computer chose: " + computerChoice
-  );
-  result = playRound(playerChoice, computerChoice);
-  if (result === "draw") {
-    console.log("You draw this round.");
-  } else if (result === "win") {
-    console.log("You win this round.");
-    playerScore++;
-  } else if (result === "lose") {
-    console.log("You lose this round.");
-    computerScore++;
-  } else {
-    console.log('Wrong input, choose: "rock", "paper" or "scissors".');
-    currentRound--;
-  }
-  currentRound++;
-  console.log(
-    "Your score: " + playerScore + "\nComputer Score: " + computerScore
-  );
-
-  if (playerScore > computerScore) {
-    console.log("You win. Well played!");
-  } else if (playerScore < computerScore) {
-    console.log("You lose. Better luck next time!");
-  } else {
-    console.log("You tied all rounds!");
-  }
-}
-
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", function () {
+  playRound("rock");
+});
+paper.addEventListener("click", function () {
+  playRound("paper");
+});
+scissors.addEventListener("click", function () {
+  playRound("scissors");
+});
